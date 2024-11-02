ef2219e59d023bb3209d38c7961a6b17b60ecc26
Богдан <boda79322@gmail.com>
2024-11-02 11:29:26 +0200

    the sims

--- python_s/P2_f1.py
+++ python_s/P2_f1.py
@@ -1,10 +1,60 @@
-class student:
-    print("hi")
-    def __init__(self, height=160):
-     self.height = height
-     print('i am alive')
+class Pet:
 
-first_student = student()
-second_student = student(height=170)
-print(first_student.height)
-print(second_student.height)
+   def __init__(self, name, species):
+
+       self.name = name
+
+       self.species = species
+
+       self.hungry = True
+
+       self.tired = True
+
+       self.happy = False
+
+
+   def feed(self):
+
+       if self.hungry:
+
+           print(f"{self.name} has been fed.")
+
+           self.hungry = False
+
+       else:
+
+           print(f"{self.name} is not hungry.")
+
+
+   def sleep(self):
+
+       if self.tired:
+
+           print(f"{self.name} has gone to sleep.")
+
+           self.tired = False
+
+       else:
+
+           print(f"{self.name} is not tired.")
+
+
+   def play(self):
+
+       if self.happy:
+
+           print(f"{self.name} is already playing.")
+
+       else:
+
+           print(f"{self.name} is playing now.")
+
+           self.happy = True
+
+
+
+
+
+
+
+
